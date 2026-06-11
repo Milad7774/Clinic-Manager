3a61db0264a75a4cc14fec02bb99e992d934931a
Milad <elsayeghmilad8@gmail.com>
2026-06-11 10:36:02 +0300

    More Fixing for appointments(time comparison included)

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -3,36 +3,50 @@ import Nolist from "./Nolist";
 
 const Appointments = () => {
   //Grab data
-  let list = JSON.parse(localStorage.getItem("mylist") || '[]');
+  let list = JSON.parse(localStorage.getItem("mylist") || "[]");
 
-  let stop = true;
+  const [notFound, setNotFound] = useState(() => true);
+
+  const [stop, setStop] = useState(() => true);
 
   let sessions = [];
 
-  list.map((itemList, indexList) =>{
-    list[indexList].sessions.map((itemSessions, indexSessoins) =>{
-      const patientSession = {
-        name: itemList.name,
-        session: itemSessions.sessionDate,
-        time: itemSessions.time,
-        phoneNumber: itemList.phoneNumber
+
+  list.map((itemList, indexList) => {
+    list[indexList].sessions.map((itemSessions, indexSessoins) => {
+      
+      //Date with time
+      let hours = itemSessions.time[0] + itemSessions.time[1];
+
+      let minutes = itemSessions.time[3] + itemSessions.time[4];
+
+      let Date_array = new Date(itemSessions.sessionDate).setHours(Number(hours), Number(minutes))
+
+      if ( Date_array >=  new Date() && stop) {
+        setStop(false);
+        setNotFound(false);
       }
-      sessions = [...sessions, patientSession]
-    })
-  })
-  console.log(sessions, "Before Sorting")
+      
+      if( Date_array >=  new Date()){
+        const patientSession = {
+          name: itemList.name,
+          session: itemSessions.sessionDate,
+          time: itemSessions.time,
+          phoneNumber: itemList.phoneNumber,
+        };
+        sessions = [...sessions, patientSession];
+      }
+    });
+  });
+  console.log(sessions, "Before Sorting");
   sessions.sort((a, b) => {
     const dateCompare = new Date(a.session) - new Date(b.session);
     if (dateCompare !== 0) return dateCompare;
     return a.time.localeCompare(b.time);
-    });
-    console.log(sessions, "After Sorting");
+  });
+  console.log(sessions, "After Sorting");
 
-
-
-  console.log(list)
-
-  const [notFound, setNotFound] = useState(list.length === 0);
+  console.log(list);
 
   return (
     <div>
@@ -51,15 +65,15 @@ const Appointments = () => {
               </tr>
             </thead>
             <tbody>
-              {sessions.map((item, index) =>{
-                return(
+              {sessions.map((item, index) => {
+                return (
                   <tr key={index}>
                     <td> {item.name} </td>
                     <td> {item.phoneNumber} </td>
                     <td> {item.session} </td>
                     <td> {item.time} </td>
                   </tr>
-                )
+                );
               })}
             </tbody>
           </table>
