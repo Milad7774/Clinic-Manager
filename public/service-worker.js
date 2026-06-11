50e08b02c018f92f3bd0cbe93e178960b4a50f87
Milad <elsayeghmilad8@gmail.com>
2026-06-11 13:59:28 +0300

    Removed More Notificatoins leftovers

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,35 +18,3 @@ self.addEventListener('fetch', (event) => {
       .then(response => response || fetch(event.request))
   );
 });
-//Notifications
-
-self.addEventListener('push', (event) => {
-    let data = {};
-  
-    if (event.data) {
-      data = event.data.json();
-    }
-  
-    const options = {
-      body: data.body || 'You have an appointment reminder',
-      icon: '/icon-192.png',
-      badge: '/badge-72.png',
-      vibrate: [200, 100, 200],
-      data: {
-        url: data.url || '/'
-      }
-    };
-  
-    event.waitUntil(
-      self.registration.showNotification(data.title || 'Dentist App', options)
-    );
-  });
-  
-  self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-  
-    const urlToOpen = event.notification.data.url;
-    event.waitUntil(
-      clients.openWindow(urlToOpen)
-    );
-  });
