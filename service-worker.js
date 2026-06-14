aca1119ae48caa4d8a3607a24a0213ff106fecd8
Milad <elsayeghmilad8@gmail.com>
2026-06-14 21:56:12 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // public/service-worker.js
 
-const CACHE_NAME = "clinic-manager-v8";
+const CACHE_NAME = "clinic-manager-v9";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
