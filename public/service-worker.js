17a9b2742b73de9b843badde64cb921f47c72eb5
Milad <elsayeghmilad8@gmail.com>
2026-06-14 14:04:57 +0300

    Updated Version to v7

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // public/service-worker.js
 
-const CACHE_NAME = "clinic-manager-v6";
+const CACHE_NAME = "clinic-manager-v7";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
