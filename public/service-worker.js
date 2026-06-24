0b05f8615b8de54b474efb90cb881fd7bf60f70d
Milad <elsayeghmilad8@gmail.com>
2026-06-24 12:21:07 +0300

    Bumbed to V14

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // public/service-worker.js
 
-const CACHE_NAME = "clinic-manager-v13";
+const CACHE_NAME = "clinic-manager-v14";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
